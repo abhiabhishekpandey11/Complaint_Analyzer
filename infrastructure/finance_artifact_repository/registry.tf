f677a6f87e7fd780d0c4efa68123e8236500e2fa
Abhishek Pandey <abhiabhishekpandey11@gmail.com>
2023-08-25 01:33:30 +0530

    updated

--- infrastructure/finance_artifact_repository/registry.tf
+++
@@ -1,9 +0,0 @@
-provider "google" {
-  project = var.project_name
-}
-
-resource "google_artifact_registry_repository" "finance_repo" {
-  location      = var.artifact_repository_location
-  repository_id = var.artifact_repository_repository_id
-  format        = var.artifact_repository_format
-}
